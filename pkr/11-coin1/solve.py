7e061324d2788ea8b4db3b1516e430ff94a3ccd0
bobgil <hg9587@naver.com>
2019-01-18 13:43:00 +0900

    code refactoring

--- pkr/11-coin1/solve.py
+++ pkr/11-coin1/solve.py
@@ -7,13 +7,6 @@ proc = remote("pwnable.kr", 9007)
 
 print proc.recvuntil("sec... -")
 
-# proc.recvuntil("N=")
-# N = proc.recvuntil(" ")
-# proc.recvuntil("C=")
-# C = proc.recvuntil("\n")
-
-# N = int(N)
-# C = int(C)
 N, C = 1, 1
 def parseInput():
 	global N
@@ -27,17 +20,12 @@ def parseInput():
 	C = int(C)
 	print "N = %d / C = %d" % (N, C)
 
-parseInput()
-
 def query(start, end):
-	# print "Query for (%d, %d)" % (start, end)
 	global proc
 	nums = [str(i) for i in range(start, end)]
 	queries = " ".join(nums) + "\n"
-	# print "queries = %s" % queries
 	proc.send(queries)
 	ret = proc.recvuntil("\n")
-	# print "ret = %s" % ret
 	try:
 		ret = int(ret)
 	except e:
@@ -57,10 +45,8 @@ def solve(N, C):
 		NumQueries = NumQueries + 1
 		res = query(left, mid)
 		if expected == res:
-			# print "Not included"
 			left = mid
 		else:
-			# print "In this range!"
 			right = mid
 		mid = (left + right) / 2
 	while NumQueries < C:
@@ -68,17 +54,13 @@ def solve(N, C):
 		NumQueries = NumQueries + 1
 	return str(left)
 
-ans = solve(N, C)
-print "ans = %s" % ans
-proc.send(ans + "\n")
-print proc.recvuntil(")")
-
-for i in range(0, 99):
+for i in range(0, 100):
 	parseInput()
 	ans = solve(N, C)
 	print "ans = %s" % ans
 	proc.send(ans + "\n")
 	print proc.recvuntil(")")
 
-
 proc.interactive()
+
+# flag = b1NaRy_S34rch1nG_1s_3asy_p3asy
